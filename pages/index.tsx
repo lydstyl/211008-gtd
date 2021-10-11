04db4c5d38bc48376bd1a4545e22abd5e2599c76
Gabriel Brun <lydstyl@gmail.com>
2021-10-11 19:08:29 +0200

    Set Jest and React Testing Library.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css"
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <h1>Gabriel&apos;s GTD method</h1>
+      <h1>GTD</h1>
 
       <h2>Collect</h2>
       <ul>
