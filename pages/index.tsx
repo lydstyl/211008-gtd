25c47f8eba172370b1aa6143a3a4c7b7e781f029
Gabriel Brun <lydstyl@gmail.com>
2021-10-11 19:00:52 +0200

    Add a vscode config file for prettier & eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,45 @@
-import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+import type { NextPage } from "next"
+import styles from "../styles/Home.module.css"
 
 const Home: NextPage = () => {
-    return (
-        <div className={styles.container}>
-            <h1>Gabriel's GTD method</h1>
+  return (
+    <div className={styles.container}>
+      <h1>Gabriel&apos;s GTD method</h1>
 
-            <h2>Collect</h2>
-            <ul>
-                <li>one</li>
-                <li>two</li>
-                <li>three</li>
-            </ul>
+      <h2>Collect</h2>
+      <ul>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+      </ul>
 
-            <h2>TODO</h2>
-            <ul>
-                <li>ONE</li>
-                <li>TWO</li>
-                <li>three</li>
-            </ul>
+      <h2>TODO</h2>
+      <ul>
+        <li>ONE</li>
+        <li>TWO</li>
+        <li>three</li>
+      </ul>
 
-            <p>Use local storage</p>
-            <p>A todo component :</p>
-            <p>must be</p>
-            <ul>
-                <li>uid</li>
-                <li>id</li>
-                <li>name</li>
-                <li>dueDate</li>
-                <li>labels including important, urgent, and so on</li>
-            </ul>
+      <p>Use local storage</p>
+      <p>A todo component :</p>
+      <p>must be</p>
+      <ul>
+        <li>uid</li>
+        <li>id</li>
+        <li>name</li>
+        <li>dueDate</li>
+        <li>labels including important, urgent, and so on</li>
+      </ul>
 
-            <p>later</p>
-            <ul>
-                <li>description</li>
-                <li>activity</li>
-                <li>checklist to make process and so on</li>
-                <li>files</li>
-            </ul>
-        </div>
-    );
-};
+      <p>later</p>
+      <ul>
+        <li>description</li>
+        <li>activity</li>
+        <li>checklist to make process and so on</li>
+        <li>files</li>
+      </ul>
+    </div>
+  )
+}
 
-export default Home;
+export default Home
