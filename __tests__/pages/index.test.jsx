2aa56f778ecc42a32bfac8bca88596c8130c6fd5
Gabriel Brun <lydstyl@gmail.com>
2021-10-12 10:26:50 +0200

    Renders collect, todo & waiting task lists.

--- __tests__/pages/index.test.jsx
+++ __tests__/pages/index.test.jsx
@@ -9,7 +9,7 @@ import { render, screen } from '@testing-library/react'
 import Home from '../../pages/index'
 
 describe('Home', () => {
-  it('renders a heading', () => {
+  it('Renders a heading.', () => {
     render(<Home />)
 
     const heading = screen.getByRole('heading', {
@@ -19,5 +19,21 @@ describe('Home', () => {
     expect(heading).toBeInTheDocument()
   })
 
-  it('')
+  it('Renders collect, todo & waiting task lists.', () => {
+    render(<Home />)
+
+    const collectList = screen.getByRole('list', {
+      name: /collect/i,
+    })
+    const todoList = screen.getByRole('list', {
+      name: /to\sdo/i,
+    })
+    const waitingList = screen.getByRole('list', {
+      name: /waiting/i,
+    })
+
+    expect(collectList).toBeInTheDocument()
+    expect(todoList).toBeInTheDocument()
+    expect(waitingList).toBeInTheDocument()
+  })
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,28 @@
-import type { NextPage } from "next"
-import styles from "../styles/Home.module.css"
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <h1>GTD</h1>
 
-      <h2>Collect</h2>
-      <ul>
-        <li>one</li>
-        <li>two</li>
-        <li>three</li>
-      </ul>
+      <h2 id='collect-task-list-heading'>Collect</h2>
+      <ul
+        aria-labelledby='collect-task-list-heading'
+        className='task-list'
+      ></ul>
 
-      <h2>TODO</h2>
-      <ul>
-        <li>ONE</li>
-        <li>TWO</li>
-        <li>three</li>
-      </ul>
+      <h2 id='todo-task-list-heading'>To do</h2>
+      <ul
+        aria-labelledby='todo-task-list-heading'
+        className='task-list'
+      ></ul>
+
+      <h2 id='waiting-task-list-heading'>Waiting</h2>
+      <ul
+        aria-labelledby='waiting-task-list-heading'
+        className='task-list'
+      ></ul>
 
       <p>Use local storage</p>
       <p>A todo component :</p>
